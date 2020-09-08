0f745dc4f069bfc5956d7abec376cf694b4dd3c5
ana-vela <ana@anavela.net>
2020-09-08 14:11:46 -0600

    testing recaptcha on contact form page

--- frontend/src/pages/contact.js
+++ frontend/src/pages/contact.js
@@ -4,32 +4,55 @@ import Layout from "../components/layout"
 
 import ContactPhoto from "./images/contact-me-photo.jpg"
 
+import { Helmet } from "react-helmet"
+
 const Contact = () => {
   return (
     <Layout>
-      <h1>Contact Me</h1>
+      <Helmet>
+        <script src="https://www.google.com/recaptcha/api.js"></script>
+      </Helmet>
+      <div className="contactPage">
+        <h1>Contact Me</h1>
 
-      <img src={ContactPhoto} alt="contact me" style={{ height: "400px" }} />
-      <form action={`${process.env.GATSBY_GETFORM_API}`} method="POST">
-        <label htmlFor="email">Email</label>
-        <br />
-        <input type="email" name="email" required />
-        <br />
-        <br />
-        <label htmlFor="name">Name</label>
-        <br />
-        <input type="text" name="name" required />
+        <img className="contactPhoto" src={ContactPhoto} alt="contact me" />
+        <form
+          className="contactForm"
+          action={`${process.env.GATSBY_GETFORM_API}`}
+          method="POST"
+        >
+          <label htmlFor="email">Email</label>
+          <br />
+          <input type="email" name="email" required />
+          <br />
+          <br />
+          <label htmlFor="name">Name</label>
+          <br />
+          <input type="text" name="name" required />
 
-        <br />
-        <br />
-        <label htmlFor="message">Message</label>
-        <br />
-        <input type="text" name="message" required />
+          <br />
+          <br />
+          <label htmlFor="message">Message</label>
+          <br />
+          <input type="text" name="message" required />
 
-        <br />
-        <br />
-        <button type="submit">Send</button>
-      </form>
+          <br />
+          <br />
+          <div
+            className="g-recaptcha"
+            data-sitekey={`${process.env.GATSBY_RECAPTCHA_API}`}
+          ></div>
+          <br />
+          <br />
+          <button
+            className="bg-transparent hover:bg-black text-white font-semibold hover:text-white py-2 px-4 border border-white hover:border-transparent rounded"
+            type="submit"
+          >
+            Send
+          </button>
+          <br />
+        </form>
+      </div>
     </Layout>
   )
 }
