46443a177403789b5442d5068177cf7149eb4d9c
ana-vela <ana@anavela.net>
2021-01-26 21:44:41 -0700

    adjust favicon links

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -44,15 +44,15 @@ module.exports = {
         background_color: "#663399",
         theme_color: "#663399",
         display: "minimal-ui",
-        icon: `src/images/my-icon.png`,
+        icon: `src/assets/favicon.png`,
         icons: [
           {
-            src: `src/images/my-icon.png`,
+            src: `favicons/icon-48X48.png`,
             sizes: `48x48`,
             type: `image/png`,
           },
           {
-            src: `src/images/my-icon.png`,
+            src: `favicons/icon-72x72.png`,
             sizes: `72x72`,
             type: `image/png`,
           },
